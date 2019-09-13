c34d71ab8c1e51c98b8be48e1c5451a06ce70c9e
Rehman Ali <rehmanali001@gmail.com>
2019-09-13 16:46:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,21 +4,13 @@ class App < Sinatra::Base
   
   get '/name' do 
     "My name is Rehman"
-    erb :'name/index.html.erb'
   end 
   
   get '/hometown' do 
     "My hometown is Houston"
-    erb :'hometown/index.html.erb'
   end
   
   get '/favorite-song' do 
     "My favorite song is One Dance"
-    erb :'favorite-song/index.html.erb'
   end 
 end
-
-  get '/hometown' do 
-    "My hometown is Houston"
-    erb :'hometown/index.html.erb'
-  end
