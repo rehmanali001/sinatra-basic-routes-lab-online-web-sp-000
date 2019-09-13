a443af67672109031db1837271fcf02e1bf10629
Rehman Ali <rehmanali001@gmail.com>
2019-09-13 16:39:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get '/name' do 
+    "My name is Rehman"
+    erb
+  end 
   
+  get '/hometown' do 
   
+  end
   
-  
-  
-  
-  
+  get '/favorite-song' do 
+    
+  end 
 end
